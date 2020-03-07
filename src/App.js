02a6c3f7cbfb2de8cf1e960728a938d326a6b0e8
Mary Huamán <fer.hs19@gmail.com>
2020-03-06 19:43:36 -0500

    Tercera clase de React

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './styles/styles.scss';
 import Curso from './Curso';
 
-import './styles/styles.scss';
+const Cursos = [
+  {
+    titulo:"React desde cero",
+    image:"https://steamuserimages-a.akamaihd.net/ugc/273969530040390215/58E29EF8863140262E52F3956A6F166C9DEE9402/",
+    price:"50",
+    profesor:"Fernanda"
+  },
+  {
+    titulo:"Html desde cero",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSDGhJ0Mm7sAcDmPgn1gvdctcAo8n-2F0xq3xCwgk_oz7xJBN6O",
+    price:"50",
+    profesor:"Sofi"
+  },
+  {
+    titulo:"JS desde cero",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
+    price:"50",
+    profesor:"Brayan"
+  }
+]
 
 const App=()=>(
 <>
@@ -18,12 +38,15 @@ const App=()=>(
     </div>
   </div>
   <div className="ed-grid m-grid-3">
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
-    <Curso />
+    {
+      Cursos.map(curso=>
+        <Curso 
+          titulo={curso.titulo} 
+          imagen={curso.imagen}
+          price = {curso.price}
+          profesor = {curso.profesor}
+        />)
+    }
   </div>
 </>
 )
--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-
-const Curso =()=>(
+import PropTypes from 'prop-types'
+//Cuando se recibe por props se recibe todo el objeto, pero esto se puede optimisar
+//escojiendo solo lo que se necesita para tu componente ({titulo,imagen,price})
+const Curso = ({titulo,imagen,price,profesor})=>(
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img src="https://definicionyque.es/wp-content/uploads/2018/10/IMAGEN-_PUBLICITARIA.jpg" alt="Poster del curso"></img>
+        <img src={imagen} alt={titulo}></img>
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">
-          Programación orientada a objetos con Go
-        </h3>
+        <h3 className="t5 s-mb-2 s-center">{titulo}</h3>
         <div className="s-mb-2 s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
@@ -16,14 +16,27 @@ const Curso =()=>(
                 <img src="https://1.bp.blogspot.com/-79DdxzZkDog/T76QV6v5IuI/AAAAAAAAAEY/6DzpGZzsmfA/s320/homerocatolico_456_336.jpg" alt=""></img>
               </div>
             </div>
-            <span className="small">Alexys Lozada</span>
+            <span className="small">{profesor}</span>
           </div>
-        </div>
+        </div>        
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny" href="https://ux.ed.team/cards.html">$ 20USD</a>
+          <a className="button--ghost-alert button--tiny" href="https://ux.ed.team/cards.html">$ {price} usd</a>
         </div>
       </div>
     </article>    
 )
 
+Curso.propTypes = {
+    titulo: PropTypes.string,
+    imagen: PropTypes.string,
+    price: PropTypes.string,
+    profesor: PropTypes.string
+}
+
+Curso.defaultProps ={
+    titulo: "No se encontró titulo",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTZWxq_DqhMbwm5yN1F3G3O7AU0r37vyj6MZOAGxj-7Gxq3Rr0R",
+    price: "20",
+    profesor: "Fer"
+}
 export default Curso
